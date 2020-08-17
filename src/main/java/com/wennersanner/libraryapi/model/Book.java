3c5276baa872f3a1742bb09438cc46ee84c82b34
wennersgc <wennersannerma@gmail.com>
2020-08-16 21:16:27 -0300

    #07 - Teste de integração para verificar se um livro com determinado ISBN existe ou não na base de dados

--- src/main/java/com/wennersanner/libraryapi/model/Book.java
+++ src/main/java/com/wennersanner/libraryapi/model/Book.java
@@ -5,12 +5,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity (name = "book")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String author;
     private String isbn;
